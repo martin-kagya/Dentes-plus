f470672883c8693f2a96cb4866fb5b19333dd612
Iddrissmus <114881166+Iddrissmus@users.noreply.github.com>
2024-07-18 13:32:11 +0000

    update

---
+++ backend/account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from .views import *
+
+urlpatterns = [
+    path('signup/', signup.as_view(), name='signup'),
+    path('login/', login.as_view(), name='login'),
+    path('logout/', logout.as_view(), name='logout'),
+    path('test-view/',test.as_view(), name='test'),
+]
