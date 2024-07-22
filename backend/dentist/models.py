ea4f19d16262281e1be21502235bd37164953160
Iddrissmus <114881166+Iddrissmus@users.noreply.github.com>
2024-07-22 22:02:02 +0000

    added doctor specialization options

--- backend/dentist/models.py
+++ backend/dentist/models.py
@@ -4,10 +4,14 @@ from django.db import models
 
 # dentist model
 class Dentist(models.Model):
+    SPECIALIZATION = (
+        ('restorative', 'Restorative'),
+        ('cosmetic', 'Cosmetic'),
+    )
     dentist_id = models.AutoField(primary_key=True)
     # user_id=models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    specialization =models.CharField(max_length=100)
+    specialization =models.CharField(max_length=100,choices=SPECIALIZATION)
 
     def __str__(self):
         return self.name
